dadc08b24b138470e8edf2c629a12fb53fa46f4c
jackyjack00 <128752975+jackyjack00@users.noreply.github.com>
2023-04-24 10:41:10 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -44,3 +44,31 @@ def compute_recalls(eval_ds: Dataset, queries_descriptors : np.ndarray, database
         visualizations.save_preds(predictions[:, :num_preds_to_save], eval_ds, output_folder, save_only_wrong_preds)
     
     return recalls, recalls_str
+
+  
+class GeMPooling(nn.Module):
+    def __init__(self, feature_size, pool_size=7, init_norm=3.0, eps=1e-6, normalize=False, **kwargs):
+        super(GeMPooling, self).__init__(**kwargs)
+        self.feature_size = feature_size  # Final layer channel size, the pow calc at -1 axis
+        self.pool_size = pool_size
+        self.init_norm = init_norm
+        self.p = torch.nn.Parameter(torch.ones(self.feature_size) * self.init_norm, requires_grad=True)
+        self.p.data.fill_(init_norm)
+        self.normalize = normalize
+        self.avg_pooling = nn.AvgPool2d((self.pool_size, self.pool_size))
+        self.eps = eps
+
+    def forward(self, features):
+        # filter invalid value: set minimum to 1e-6
+        # features-> (B, H, W, C)
+        features = features.clamp(min=self.eps).pow(self.p)
+        features = features.permute((0, 3, 1, 2))
+        features = self.avg_pooling(features)
+        features = torch.squeeze(features)
+        features = features.permute((0, 2, 3, 1))
+        features = torch.pow(features, (1.0 / self.p))
+        # unit vector
+        if self.normalize:
+            features = F.normalize(features, dim=-1, p=2)
+        return features
+  
