2bb265801390e5881cbcad76d851508513f6c570
jackyjack00 <128752975+jackyjack00@users.noreply.github.com>
2023-05-12 15:44:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -167,9 +167,9 @@ def get_datasets_and_dataloaders(args, bank=None):
     # Define dataloaders, train one has with proxy and without proxy case
     if bank is not None:
         my_proxy_sampler = my_blocks.ProxyBankBatchMiner( train_dataset, args.batch_size , bank )
-        train_loader = DataLoader(dataset=train_dataset, batch_sampler = my_proxy_sampler, num_workers=args.num_workers, shuffle=True)
+        train_loader = DataLoader(dataset=train_dataset, batch_sampler = my_proxy_sampler, num_workers=args.num_workers)
     else:
-        train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, num_workers=args.num_workers)
+        train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True)
     val_loader = DataLoader(dataset=val_dataset, batch_size=args.batch_size, num_workers=4, shuffle=False)
     test_loader = DataLoader(dataset=test_dataset, batch_size=args.batch_size, num_workers=4, shuffle=False)
     return train_dataset, val_dataset, test_dataset, train_loader, val_loader, test_loader
