c5515bd5b4934615fda35eb1f9e8147c50950839
jackyjack00 <128752975+jackyjack00@users.noreply.github.com>
2023-05-14 13:11:16 +0200

    Update my_blocks.py

--- my_blocks.py
+++ my_blocks.py
@@ -247,7 +247,7 @@ class ProxyBankBatchMiner(Sampler):
             random_indeces_perm = torch.randperm( len( self.dataset ) )
             # Generate a fixed size partitioning of indeces
             batches =  torch.split( random_indeces_perm , self.batch_size )
-            print( batches )
+            print( batches.size() )
             batches_iterable = iter(batches)
         # Epochs where Bank is informative, after epoch 0
         else:
