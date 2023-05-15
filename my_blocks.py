4d53bb149295d6051bab3ab32a016aa5f03384e5
jackyjack00 <128752975+jackyjack00@users.noreply.github.com>
2023-05-15 17:18:02 +0200

    Update my_blocks.py

--- my_blocks.py
+++ my_blocks.py
@@ -247,7 +247,6 @@ class ProxyBankBatchMiner(Sampler):
         
     # Return an iterable over a list of groups of indeces (list of batches)
     def __iter__(self): 
-        print(f"Counter : {self.counter}")
         # Epoch 0 case
         if self.is_first_epoch and self.counter % 2 == 0:
             # Change flag, first epoch is done
@@ -257,12 +256,13 @@ class ProxyBankBatchMiner(Sampler):
             # Generate a fixed size partitioning of indeces
             batches =  torch.split( random_indeces_perm , self.batch_size )
             self.batch_iterable = iter(batches)
+            print(f"Counter : {self.counter}")
         # Epochs where Bank is informative, after epoch 0
         elif self.counter % 2 == 0:
             # Generate batches from ProxyBank
             batches = self.bank.batch_sampling( self.batch_size )
             self.batch_iterable = iter(batches)
-        print(f"Counter : {self.counter}")
+            print(f"Counter : {self.counter}")
         self.counter += 1
         return  self.batch_iterable
     
