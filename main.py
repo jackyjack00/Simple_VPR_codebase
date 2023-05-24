961c14f0e3f9b78f270b2722188ff05b5c92e458
jackyjack00 <128752975+jackyjack00@users.noreply.github.com>
2023-05-24 11:17:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ class LightningModel(pl.LightningModule):
         elif  self.optimizer_str == "nadam":
             optimizer = torch.optim.NAdam(self.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, momentum_decay=0.004)
             
-        if self.lr_scheduler == "exponential" :
+        if self.lr_scheduler_str == "exponential" :
             scheduler = [ torch.optim.lr_scheduler.ExponentialLR(optimizers, gamma = .95) ]
         else :
             scheduler = []
