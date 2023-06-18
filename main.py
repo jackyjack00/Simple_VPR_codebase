3cda950bfeedb080bf1f9eb8efd0ea50df4c246f
jackyjack00 <128752975+jackyjack00@users.noreply.github.com>
2023-06-18 20:21:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -181,6 +181,9 @@ class LightningModel(pl.LightningModule):
         print(recalls_str)
         self.log('R@1', recalls[0], prog_bar=False, logger=True)
         self.log('R@5', recalls[1], prog_bar=False, logger=True)
+        #added
+        self.log('R@10', recalls[2], prog_bar=False, logger=True)
+        self.log('R@20', recalls[3], prog_bar=False, logger=True)
 
 def get_datasets_and_dataloaders(args, bank = None):
     # Define Transformation to apply
